a4f87c871c15749d7ac78e1253d783e645b8f665
AaronChen <aaronchen@aaronchendembp.lan>
2014-11-05 22:40:57 +0800

    init the project

---
+++ server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env/ python
+#coding:utf-8
+
+import tornado.ioloop
+import tornado.options
+import tornado.httpserver
+
+import sys
+
+from application import application
+from tornado.options import define,options
+define("port",default=8888,help="run on the given port",type=int)
+
+def main():
+	tornado.options.parse_command_line()
+	http_server = tornado.httpserver.HTTPServer(application)
+	http_server.listen(options.port)
+	print "On Develop Status... running at localhost:%s/" %options.port
+	print "Quit the Server with Contor-C"
+	tornado.ioloop.IOLoop.instance().start()
+
+if_name_ == "_main_":
+	main()
