0c3bb2cb0619ae08874e0d34d099ece6a208ae21
AaronChen <aaronchen@aaronchendembp.lan>
2014-11-06 20:45:42 +0800

    add some handler

--- url.py
+++ url.py
@@ -6,8 +6,13 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 from handler.index import indexHandler
+from handler.login import loginHandler
+from handler.build import buildHandler
+
+
 
 url=[
-	(r'/',indexHandler)
-
+	(r'/',indexHandler),
+	(r'/login',loginHandler),
+	(r'/build',buildHandler)
 ]
