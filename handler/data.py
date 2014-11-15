aff33e0ac026ced8e5a38c709cf7db81d4d05c10
Aaronchen <chen.dong85@163.com>
2014-11-15 12:17:19 +0800

    add the database handler

--- handler/data.py
+++ handler/data.py
@@ -1,19 +1,48 @@
 #!/usr/bin/env python
-#coding:utf-8
+# coding:utf-8
 
 import tornado.web
 import json
 import sys
+
 reload(sys)
-sys.setdefaultencoding('utf-8') 
+sys.setdefaultencoding('utf-8')
 from optsql.database import dbHander
 
+
 class dataHandler(tornado.web.RequestHandler):
-	def post(self):
-		requestPage = self.get_arguments("data");
-		# print requestPage
-		if (requestPage == [u"VM"]):
-			dbdata = dbHander().getData("vm")
-			vmdata = json.dumps(dbHander().transformData(dbdata,"vm"))
-			self.write(vmdata)
-		
+    def post(self):
+        requestType = self.get_argument("requestType")[0]
+        requestPage = self.get_arguments("page")[0]
+        requestData = self.get_argument("data")[0]
+        if(requestType == u"read"):
+            requestData = self.get_argument("data")[0]
+            dbData = dataHandler().getData(requestPage, requestData)
+            clientdata = json.dumps(dataHandler().transformData(dbData, requestPage))
+            self.write(clientdata)
+            return False
+        if(requestType == u"write"):
+            dbData = json.loads(requestData)
+            result = dbHander().wirteData(requestPage, dbData)
+            if(result == True):
+                self.write({"result":"success"})
+            else:
+                self.write({"result":"fail"})
+            return False
+        if(requestType == u"update"):
+            dbData = json.loads(requestData)
+            result = dbHander().updateData(requestPage, dbData)
+            if(result == True):
+                self.write({"result":"success"})
+            else:
+                self.write({"result":"fail"})
+            return False
+        if(requestType == u"delete"):
+            dbData = json.loads(requestData)
+            result = dbHander().deleteData(requestPage, dbData)
+            if(result == True):
+                self.write({"result":"success"})
+            else:
+                self.write({"result":"fail"})
+            return False
+
--- optsql/database.py
+++ optsql/database.py
@@ -15,12 +15,45 @@ class dbHander():
     def getData(self, table, num):
         if(num == 'all'):
             query = ("select * from " + table)
+        else:
+            query = ("select * from " + table + "where id = " + num)
+        try:
             cur.execute(query)
             data = cur.fetchall()
-            conn.close()
-            return data;
+        except:
+            conn.rollback()
 
-    # def writeData(self,table,data):
+        conn.close()
+        return data
+
+    def writeData(self, table, data):
+        items = data.keys()
+        values = data.values()
+        itemstr = data.keys()[0]
+        valuestr = data.values()[0]
+        for i in range(1,len(items)):
+            itemstr = itemstr + ", " + items[i]
+        for j in range(1,len(values)):
+            valuestr = valuestr + ", " + values[i]
+
+        query = ("insert into " + table + "(" + itemstr +")" +"values" + "("+ valuestr +")" )
+        try:
+            cur.execute(query)
+            cur.commit()
+            conn.close()
+            return  True
+        except:
+            conn.rollback()
+            conn.close()
+            return False
+
+
+    def updateData(self, table, data):
+
+        return True
+    def deleteData(self, table, data):
+
+        return True
 
     def transformData(self, dbdata, page):
         if (len(dbdata) == 0):
