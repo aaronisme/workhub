a33aef4e5dc6c238c9b4484a5f378a2f18a5c3e7
Aaronchen <chen.dong85@163.com>
2014-11-15 10:41:58 +0800

    add the database handler

--- handler/data.py
+++ handler/data.py
@@ -6,32 +6,14 @@ import json
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8') 
-from database import
-
-
-vmdataobj = {
-	"response": True,
-	"hasData": True,
-	"tabName":"id,ip,user,Occupied,Build,Description",
-	"data" : [
-		[1,"9.110.94.110:8080","QC","M18","10.2.1003.156","Capsian xxx"],
-		[2,"9.110.94.110:8080","QC","M18","10.2.1003.156","Capsian xxx"],
-		[3,"9.110.94.110:8080","QC","M18","10.2.1003.156","Capsian xxx"],
-		[4,"9.110.94.110:8080","QC","M18","10.2.1003.156","Capsian xxx"],
-		[5,"9.110.94.110:8080","QC","M18","10.2.1003.156","Capsian xxx"],
-		[6,"9.110.94.110:8080","QC","M18","10.2.1003.156","Capsian xxx"],
-		[7,"9.110.94.110:8080","QC","M18","10.2.1003.156","Capsian xxx"],
-		[8,"9.110.94.110:8080","QC","M18","10.2.1003.156","Capsian xxx"],
-		[9,"9.110.94.110:8080","QC","M18","10.2.1003.156","Capsian xxx"]
-	]
-}
-
-vmdata = json.dumps(vmdataobj)
+from optsql.database import dbHander
 
 class dataHandler(tornado.web.RequestHandler):
 	def post(self):
 		requestPage = self.get_arguments("data");
 		# print requestPage
 		if (requestPage == [u"VM"]):
+			dbdata = dbHander().getData("vm")
+			vmdata = json.dumps(dbHander().transformData(dbdata,"vm"))
 			self.write(vmdata)
 		
--- optsql/database.py
+++ optsql/database.py
@@ -1,10 +1,41 @@
 #!/usr/bin/env python
-#coding:utf-8
+# coding:utf-8
 
-import MySQLdb
+import mysql.connector
 import json
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8') 
 
-conn = MySQLdb.connect(host = "")
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+conn = mysql.connector.connect(user='root', password='123456', database='workhub')
+cur = conn.cursor()
+
+class dbHander():
+    def getData(self, table, num):
+        if(num == 'all'):
+            query = ("select * from " + table)
+            cur.execute(query)
+            data = cur.fetchall()
+            conn.close()
+            return data;
+
+    # def writeData(self,table,data):
+
+    def transformData(self, dbdata, page):
+        if (len(dbdata) == 0):
+            requestData = {
+                "response": True,
+                "hasData": False,
+            }
+            return requestData
+        else:
+            if (page == "vm"):
+                requestData = {
+                    "response": True,
+                    "hasData": True,
+                    "tabName": "id,ip,user,Occupied,Build,Description",
+                    "data": dbdata
+                }
+                return requestData
+
